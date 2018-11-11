dc8511259898488fd2927f193d27f6a8dd004a21
Blazon <blazonmediaco@gmail.com>
2018-11-11 15:45:30 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// control the auto refresh rate -> 1000 = 1 second
+var refreshRate = 15 * 10000; // 15 Minutes
+
+
+// Automatically reload the browser
+setInterval(function() {
+   window.location.reload();
+}, refreshRate);
+
+// Hide the images on load
+$('#blazon').hide();
+
+// Get a random image from Unsplash and fade it in on load
+$('#blazon').css({
+  // serves a random image from this collection on Unsplash.com
+  'background-image' : 'url(https://source.unsplash.com/collection/495468/1600x900)'
+}).fadeIn(1);
